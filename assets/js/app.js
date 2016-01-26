27a309d2c1112a36ef56b7eeb520f08396c1d0bb
Kyle Dreger <kyle@dreger.me>
2016-01-26 12:44:28 -0500

    Simple home.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,26 +3,26 @@
 // Toggle night theme
 
 
-var d = document.documentElement,
-nightTime = localStorage.getItem("nightTime");
-
-if(nightTime == "true") {
-  d.classList.add("night");
-} else {
-  d.classList.remove("night");
-}
-
-window.onkeyup = function(e) {
- var key = e.keyCode ? e.keyCode : e.which;
- if (key == 76) { switchLights(); }
-}
-
-function switchLights(){
-  if(d.classList.contains("night")) {
-    d.classList.remove("night");
-    localStorage.setItem("nightTime", "false");
-  } else {
-    d.classList.add("night");
-    localStorage.setItem("nightTime", "true");
-  }
-};
+// var d = document.documentElement,
+// nightTime = localStorage.getItem("nightTime");
+// 
+// if(nightTime == "true") {
+//   d.classList.add("night");
+// } else {
+//   d.classList.remove("night");
+// }
+// 
+// window.onkeyup = function(e) {
+//  var key = e.keyCode ? e.keyCode : e.which;
+//  if (key == 76) { switchLights(); }
+// }
+// 
+// function switchLights(){
+//   if(d.classList.contains("night")) {
+//     d.classList.remove("night");
+//     localStorage.setItem("nightTime", "false");
+//   } else {
+//     d.classList.add("night");
+//     localStorage.setItem("nightTime", "true");
+//   }
+// };
