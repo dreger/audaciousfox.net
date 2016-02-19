69ad020d69d7c0824db6c5622b418c44aa91b75b
Kyle Dreger <kyle@dreger.me>
2016-02-18 21:15:24 -0500

    Firewatch Review.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,26 +3,26 @@
 // Toggle night theme
 
 
-// var d = document.documentElement,
-// nightTime = localStorage.getItem("nightTime");
-// 
-// if(nightTime == "true") {
-//   d.classList.add("night");
-// } else {
-//   d.classList.remove("night");
-// }
-// 
-// window.onkeyup = function(e) {
-//  var key = e.keyCode ? e.keyCode : e.which;
-//  if (key == 76) { switchLights(); }
-// }
-// 
-// function switchLights(){
-//   if(d.classList.contains("night")) {
-//     d.classList.remove("night");
-//     localStorage.setItem("nightTime", "false");
-//   } else {
-//     d.classList.add("night");
-//     localStorage.setItem("nightTime", "true");
-//   }
-// };
+var d = document.documentElement,
+nightTime = localStorage.getItem("nightTime");
+
+if(nightTime == "false") {
+  d.classList.remove("night");
+} else {
+  d.classList.add("night");
+}
+
+var lightSwitch = document.getElementById("light-switch");
+lightSwitch.onclick = switchLights;
+
+function switchLights(){
+  if(d.classList.contains("night")) {
+    // lightSwitch.innerHTML = "&#x1f319U+FE0E;"
+    d.classList.remove("night");
+    localStorage.setItem("nightTime", "false");
+  } else {
+    // lightSwitch.innerHTML = "&#9728;"
+    d.classList.add("night");
+    localStorage.setItem("nightTime", "true");
+  }
+};
