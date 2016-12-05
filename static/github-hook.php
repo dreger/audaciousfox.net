ef3bb64ddff797fc0e681e124ea550a8c62c8b18
K.Q. Dreger <kqdreger@users.noreply.github.com>
2016-12-04 20:15:57 -0500

    Permissions.

--- static/github-hook.php
+++ static/github-hook.php
@@ -1,10 +1,12 @@
 <?php
   exec('cd /var/www/audaciousfox.net ; git pull ; hugo', $output, $return);
+  echo shell_exec('whoami');
 
-  // Return will return non-zero upon an error
-  if (!$return) {
-      echo "Successfully";
-  } else {
-      echo "Nope.";
-  }
+
+  // // Return will return non-zero upon an error
+  // if (!$return) {
+  //     echo "Successfully";
+  // } else {
+  //     echo "Nope.";
+  // }
 ?>
