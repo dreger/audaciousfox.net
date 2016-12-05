6c6b7cdce0f24a84ab68185f4d2570f5be9529cb
K.Q. Dreger <kqdreger@users.noreply.github.com>
2016-12-04 20:50:34 -0500

    Test.

--- static/github-hook.php
+++ static/github-hook.php
@@ -1,4 +1,14 @@
-<?php 
-  exec('cd /var/www/audaciousfox.net ; git pull ; hugo ;', $output);
-  var_dump($output);
+
+
+<?php
+exec('cd /var/www/audaciousfox.net ; git pull ; hugo ;', $output, $return);
+
+// Return will return non-zero upon an error
+if (!$return) {
+    echo "PDF Created Successfully";
+    echo $output;
+} else {
+    echo "PDF not created";
+    echo $output;
+}
 ?>
