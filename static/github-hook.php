a48d1b2027cec2888c0a47855b9163f51687d1a5
K.Q. Dreger <kqdreger@users.noreply.github.com>
2016-12-04 20:30:05 -0500

    Update PHP file.

--- static/github-hook.php
+++ static/github-hook.php
@@ -1,12 +1,3 @@
 <?php
-  exec('cd /var/www/audaciousfox.net ; git pull ; hugo', $output, $return);
-  echo shell_exec('whoami');
-
-
-  // // Return will return non-zero upon an error
-  // if (!$return) {
-  //     echo "Successfully";
-  // } else {
-  //     echo "Nope.";
-  // }
+  exec('cd /var/www/audaciousfox.net ; git pull ; hugo');
 ?>
