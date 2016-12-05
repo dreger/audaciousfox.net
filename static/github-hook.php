6d6983dfdf40f82c8bee5ad0bf9e48b08a731b4f
K.Q. Dreger <kqdreger@users.noreply.github.com>
2016-12-04 20:13:11 -0500

    Test case.

--- static/github-hook.php
+++ static/github-hook.php
@@ -1,7 +1,10 @@
 <?php
-if(exec('echo EXEC') == 'EXEC'){
-    echo 'exec works';
-}
-    exec('cd /var/www/audaciousfox.net ; git pull ; hugo');
-?>
+  exec('cd /var/www/audaciousfox.net ; git pull ; hugo', $output, $return);
 
+  // Return will return non-zero upon an error
+  if (!$return) {
+      echo "Successfully";
+  } else {
+      echo "Nope.";
+  }
+?>
