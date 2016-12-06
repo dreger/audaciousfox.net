c2c6ebb01d2a395e2b1304a23111c2db35650483
K.Q. Dreger <kqdreger@users.noreply.github.com>
2016-12-06 12:57:08 -0500

    Test push.

--- static/github-hook.php
+++ static/github-hook.php
@@ -2,13 +2,4 @@
 
 <?php
 exec('cd /var/www/audaciousfox.net ; git pull ; hugo ;', $output, $return);
-
-// Return will return non-zero upon an error
-if (!$return) {
-    echo "PDF Created Successfully";
-    echo $output;
-} else {
-    echo "PDF not created";
-    echo $output;
-}
 ?>
