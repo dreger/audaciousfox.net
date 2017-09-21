18700c48ddc4d60d24021208bab72e23b3305a6f
K.Q. Dreger <dreger@users.noreply.github.com>
2017-09-21 19:17:33 -0400

    Remove secret github check

--- static/github-hook.php
+++ static/github-hook.php
@@ -1,27 +1,27 @@
 <?php 
-	$secret = $_ENV("SECRET_TOKEN");
+	// $secret = $_ENV("SECRET_TOKEN");
 	 
-	$headers = getallheaders();
-	$hubSignature = $headers['X-Hub-Signature'];
+	// $headers = getallheaders();
+	// $hubSignature = $headers['X-Hub-Signature'];
 
-	// Split signature into algorithm and hash
-	list($algo, $hash) = explode('=', $hubSignature, 2);
+	// // Split signature into algorithm and hash
+	// list($algo, $hash) = explode('=', $hubSignature, 2);
 	 
-	// Get payload
-	$payload = file_get_contents('php://input');
+	// // Get payload
+	// $payload = file_get_contents('php://input');
 	 
-	// Calculate hash based on payload and the secret
-	$payloadHash = hash_hmac($algo, $payload, $secret);
+	// // Calculate hash based on payload and the secret
+	// $payloadHash = hash_hmac($algo, $payload, $secret);
 
-	echo $hash;
-	echo "\n";
-	echo $payloadHash;
+	// echo $hash;
+	// echo "\n";
+	// echo $payloadHash;
 	 
-	// Check if hashes are equivalent
-	if ($hash !== $payloadHash) {
-	    // Kill the script or do something else here.
-	    die('Bad secret');
-	}
+	// // Check if hashes are equivalent
+	// if ($hash !== $payloadHash) {
+	//     // Kill the script or do something else here.
+	//     die('Bad secret');
+	// }
 	 
 	// Your code here.
 	// $data = json_decode($payload);
